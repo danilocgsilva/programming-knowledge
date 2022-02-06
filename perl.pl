fbfeb6a4a4ea0472619afb97878f83ab0b737a01
Danilo Silva <danilocgsilva@gmail.com>
2022-02-06 08:46:42 -0300

    Added perl exercise

---
+++ perl.pl
@@ -0,0 +1,10 @@
+# Perl exercises
+
+1. This script has the duty to print all results of 2 powers all numbers from 1 to 16. But is is wrong! Fix it.
+
+```perl
+for ($i = 1; $i <= 16; $i++) {
+    $result = 2 << $i;
+    print("2 powers $i is " . $resultado . "\n");
+}
+```
